ad2ede966727325e86b0bf32c0eb7fdf6f43498d
Ben Kartchner <brkartch@gmail.com>
2023-03-05 20:28:15 -0700

    added stuff to play and scores js

---
+++ scores.js
@@ -0,0 +1,23 @@
+
+/*
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+        scores = JSON.parse(scoresText);
+    }
+
+    const tableBodyEl = document.querySelector('#scores');
+
+    if(scores.length){
+        for (const [i, score] of scores.entries()) {
+            const positionTdEl = document.createElement('td');
+            const nameTdEl = document.createElement('td');
+            const scoreTdEl = document.createElement('td');
+            const dateTdEl = document.createElement('td');
+
+            positionTdEl.textContent
+        }
+    }
+}
+*/
