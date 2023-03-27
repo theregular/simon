eb7962c1719c03d636f845f581714caa0e145017
Ben Kartchner <brkartch@gmail.com>
2023-03-26 23:09:58 -0600

    updated to have login work with services

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -13,7 +15,30 @@ if (!userName) {
 const url = `mongodb+srv://${userName}:${password}@cluster0.bxy95fo.mongodb.net/`;
 
 const client = new MongoClient(url);
+const userCollection = client.db('simon').collection('user');
 const scoreCollection = client.db('simon').collection('score');
+
+function getUser(email) { //finds user by email from DB
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) { //finds user by authToken from DB
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
 
 function addScore(score) {
   scoreCollection.insertOne(score);
@@ -29,5 +54,11 @@ function getHighScores() {
   return cursor.toArray();
 }
 
-module.exports = { addScore, getHighScores };
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addScore,
+  getHighScores,
+};
 
